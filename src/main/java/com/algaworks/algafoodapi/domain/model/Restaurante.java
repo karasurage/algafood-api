a89df7be7083c4ba4de0a80b8afd82ec9b23c2a5
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-04-26 08:37:51 -0300

    Tratando erros com mensagens de validação

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -38,11 +38,12 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Nome é obrigatório")
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
-    @PositiveOrZero
+    @NotNull // adicionei porque o @PositiveOrZero não valida se é nulo
+    @PositiveOrZero(message = "{TaxaFrete.invalida}")
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
