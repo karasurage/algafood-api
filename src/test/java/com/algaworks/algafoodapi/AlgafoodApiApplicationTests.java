4f4cbb76d5b28e2f3fee5451b554e37e324213ed
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-05-08 14:35:07 -0300

    Implementacao testes de integracao e de api

--- src/test/java/com/algaworks/algafoodapi/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafoodapi/AlgafoodApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.algaworks.algafoodapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AlgafoodApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
