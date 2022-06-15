f10ea03e0530731ffca88de15d5b403da0fd9ce4
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-06-15 08:36:26 -0300

    Ajustes nas mensagens.properties para o modelo de restauranteInput

--- src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisassembler.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.api.assembler;
 
 import com.algaworks.algafoodapi.api.model.input.RestauranteInput;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestauranteInputDisassembler {
 
+    private final ModelMapper modelMapper;
+
     @Autowired
-    private ModelMapper modelMapper;
+    public RestauranteInputDisassembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
         return modelMapper.map(restauranteInput, Restaurante.class);
     }
+
+    public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+//        restaurante.setCozinha(new Cozinha());
+        modelMapper.map(restauranteInput, restaurante);
+    }
+
 
 }
